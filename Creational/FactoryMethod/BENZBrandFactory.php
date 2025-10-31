9fbc92a949956ce57ebdc32f6be5db36712ea441
ahmed777ismail <ahmed1kaballo@gmail.com>
2025-10-31 06:18:49 +0200

    Implement Factory Method pattern for BMW and BENZ brands

---
+++ Creational/FactoryMethod/BENZBrandFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Creational\FactoryMethod;
+
+
+class BENZBrandFactory implements BrandFactory
+{
+    public function BuildBrand()
+    {
+        // TODO: Implement BuildBrand() method.
+        return new BENZBrand();
+    }
+}
---
+++ Creational/FactoryMethod/BMWBrand.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Creational\FactoryMethod;
+
+
+class BMWBrand implements CarBrandInterface
+{
+    public function createBrand()
+    {
+        return "BMW Brand";
+    }
+}
